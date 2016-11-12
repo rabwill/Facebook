85b78ec35dff6730bc212a99b6dd449145ee72cc
Rabia Williams <rabiawilliams@gmail.com>
2016-11-12 10:02:05 +0400

    Added Directive to block special characters

---
+++ FaceBook/Scripts/app/Directives.js
@@ -0,0 +1,18 @@
+﻿app.directive('noSpecialChar', function () {
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        link: function (scope, element, attrs, modelCtrl) {
+            modelCtrl.$parsers.push(function (inputValue) {
+                if (inputValue == null)
+                    return ''
+                cleanInputValue = inputValue.replace(/[^\w\s]/gi, '');
+                if (cleanInputValue != inputValue) {
+                    modelCtrl.$setViewValue(cleanInputValue);
+                    modelCtrl.$render();
+                }
+                return cleanInputValue;
+            });
+        }
+    }
+});
